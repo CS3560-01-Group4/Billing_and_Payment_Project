d57acd1a7422c46bd3ba173cc308fbf34185120f
David Lara <dlara7913@gmail.com>
2023-03-16 18:47:15 -0700

    Added members and methods to Membership.java.java

--- Membership.java
+++ Membership.java
@@ -7,8 +7,52 @@
  *
  */
 public class Membership {
-    int MembershipID;
+    int MembershipID, monthlyBalance;
     String MembershipStartDate,MembershipRenewalDate;
     boolean membershipActive;
+
+    Addon[] listOfAddons;
+
+
+    public int getMembershipID() {
+        return MembershipID;
+    }
+
+    public void setMembershipID(int membershipID) {
+        MembershipID = membershipID;
+    }
+
+    public String getMembershipStartDate() {
+        return MembershipStartDate;
+    }
+
+    public void setMembershipStartDate(String membershipStartDate) {
+        MembershipStartDate = membershipStartDate;
+    }
+
+    public String getMembershipRenewalDate() {
+        return MembershipRenewalDate;
+    }
+
+    public void setMembershipRenewalDate(String membershipRenewalDate) {
+        MembershipRenewalDate = membershipRenewalDate;
+    }
+
+    public boolean isMembershipActive() {
+        return membershipActive;
+    }
+
+    public void setMembershipActive(boolean membershipActive) {
+        this.membershipActive = membershipActive;
+    }
+
+    public int getMonthlyBalance() {
+        return monthlyBalance;
+    }
+
+    public void setMonthlyBalance(int monthlyBalance) {
+        this.monthlyBalance = monthlyBalance;
+    }
+
 
 }
