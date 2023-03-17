5ceb1895cf72d613767a1c1510542cc438e22637
cvoneul <cvoneul@gmail.com>
2023-03-17 02:32:16 -0700

    added getters and setters methods

--- Addon.java
+++ Addon.java
@@ -8,4 +8,34 @@
  */
 public class Addon {
 
+	private int addonID; //key identifier
+	private String name;
+	private String description;
+	private double price;
+	
+	public int getAddonID() {
+		return addonID;
+	}
+	public void setAddonID(int addonID) {
+		this.addonID = addonID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 }
