bd897b52723f0e2117fc0bffab4d8083b6dd006a
Kevin Hoang <hevinkoang@gmail.com>
2023-04-17 21:03:58 -0700

    Updated sign on page

--- SignUpPage.java
+++ SignUpPage.java
@@ -2,15 +2,18 @@ import javax.swing.*;
 
 public class SignUpPage extends JFrame {
     private JPanel SignUpPage;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTextField textField7;
-    private JPasswordField passwordField1;
+    private JTextField firstName;
+    private JTextField lastName;
+    private JTextField email;
+    private JTextField address;
+    private JTextField city;
+    private JTextField zip;
+    private JTextField state;
+    private JPasswordField password;
     private JButton nextButton;
+    private JTextField phoneNumber;
+    private String inputFirst, inputLast, inputEmail, inputAddress, inputCity, inputZip, inputState, inputPassword,
+    inputPhone;
 
     SignUpPage(){
         this.setContentPane(SignUpPage);
@@ -19,5 +22,14 @@ public class SignUpPage extends JFrame {
         this.setSize(600,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+        inputFirst = firstName.getText();
+        inputLast = lastName.getText();
+        inputEmail = email.getText();
+        inputAddress = address.getText();
+        inputCity = city.getText();
+        inputZip = zip.getText();
+        inputState = state.getText();
+        inputPhone = phoneNumber.getText();
+        inputPassword = String.valueOf(password.getPassword());
     }
 }
