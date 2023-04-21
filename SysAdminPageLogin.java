25b677bd8d91f4e1c41604e09e8e6910a3ed8aa9
Andy Chen <acchen@cpp.edu>
2023-04-21 02:49:29 -0700

    added ui for admin login

--- SysAdminPageLogin.java
+++ SysAdminPageLogin.java
@@ -1,40 +1,70 @@
 import javax.swing.*;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SysAdminPageLogin extends JFrame{
-    private JTextField usernameField;
-    private JLabel passwordLabel;
-
-    private String retrievedPassword, retreviedUsername;
-    private JLabel usernameLabel;
-    private JButton loginButton;
-    private JPasswordField passwordField;
-    private JPanel SysAdminLogin;
-
+public class SysAdminPageLogin{
+    
+    // window components
+    private JTextField usernameField = new JTextField();
+    private JPasswordField passwordField = new JPasswordField();
+    private JButton loginButton = new JButton("Login");
+    private JButton signUpButton = new JButton("Sign Up");
+    private JLabel usernameLabel = new JLabel("Enter username");
+    private JLabel passwordLabel = new JLabel("Enter password");
+    
     SysAdminPageLogin() {
-        this.setContentPane(SysAdminLogin);
-        this.setTitle("System Admin Login");
-        this.setIconImage(new ImageIcon("31-hour.png").getImage());
-        this.setSize(600, 600);
+        // create and setup window
+        JFrame frame = new JFrame("Admin Login");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,600);
+        //frame.setResizable(false);
+        frame.setIconImage(new ImageIcon("31-hour.png").getImage());
 
-        retreviedUsername = String.valueOf(passwordField.getPassword());
-        retrievedPassword = usernameField.getText();
+        // set layout manager to boxlayout
+        Container panel = frame.getContentPane();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setMaximumSize(new Dimension(100, 100));
 
+        // set component allignment
+        usernameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        passwordLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        signUpButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        usernameField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        passwordField.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // set max size of textfield
+        usernameField.setMaximumSize(new Dimension(200, 25));
+        passwordField.setMaximumSize(new Dimension(200, 25));
+
+        // add window content
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(loginButton);
+        panel.add(signUpButton);
 
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                SysAdminGUI sysAdminGUI = new SysAdminGUI();
-
-                /*
-                Add in code for login verification
-                 */
-
+                // user login here
             }
         });
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-    }
 
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new SignUpPage();
+            }
+        });
+        
+        // display windoow
+        frame.setVisible(true);
+    }
 }
