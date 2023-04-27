e7e431cef6b344a6e310afedcd4d1a69f3323ef3
cvoneul <cvoneul@gmail.com>
2023-04-26 19:38:24 -0700

    fixed Address attributes

--- Address.java
+++ Address.java
@@ -1,5 +1,5 @@
 public class Address {
-    int streetNumber;
+    int addressID;
     String streetName, city, zipCode, state;
 
     public Address(String inputAddress, String inputCity, String inputZip, String inputState) {
@@ -9,37 +9,37 @@ public class Address {
         inputState = state;
     }
 
-    public int getStreetNumber() {
-        return streetNumber;
+    public int getAddressID() {
+        return addressID;
+    }
+    public void setAddressID(int addressID) {
+        this.addressID = addressID;
     }
 
-    public void setStreetNumber(int streetNumber) {
-        this.streetNumber = streetNumber;
-    }
 
     public String getStreetName() {
         return streetName;
     }
-
     public void setStreetName(String streetName) {
         this.streetName = streetName;
     }
 
+
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
 
+
     public String getZipCode() {
         return zipCode;
     }
-
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
+
 
     public String getState() {
         return state;
