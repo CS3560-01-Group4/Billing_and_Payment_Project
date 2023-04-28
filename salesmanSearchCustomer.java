3c3797d70c3799152a0780b375e534c30039d975
cyberdremer <dlara7913@gmail.com>
2023-04-28 16:40:06 -0700

    -Added form for salesman to search for customer

---
+++ salesmanSearchCustomer.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class salesmanSearchCustomer extends JFrame {
+    String username, password;
+    private JTextField usernameText;
+    private JTextField passwordText;
+    private JButton search;
+
+    boolean completed = false;
+
+    String url = "jdbc:mysql://" + "containers-us-west-34.railway.app" + ":" + 5939 + "/" + "gymmembership";
+
+    private Connection connection = DriverManager.getConnection(url, "root", "91laqZk1CB5VM13WltEE");;
+    /*
+    Function overloading that is context based, use an INT if you plan to view/edit a customer's account;
+    A 1 allows you to view an account, a 2 allows you to edit an account's address
+     */
+
+    salesmanSearchCustomer(int Num) throws SQLException {
+        username = usernameText.getText();
+        password = passwordText.getText();
+        if (username.equals("")){
+            usernameText.setBackground(new Color(255, 102, 102));
+        } else if (password.equals("")) {
+            passwordText.setBackground(new Color(255, 102, 102));
+
+        }
+
+        if(username.equals("") || password.equals("")){
+            JOptionPane.showMessageDialog(this, "Invalid input");
+
+        }
+
+
+        search.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    DatabaseManager dbConnection = new DatabaseManager();
+                    Customer searchCustomer = dbConnection.getCustomerCredentials(username,password);
+                    if (searchCustomer == null){
+
+                    }
+                    else{
+                       switch (Num){
+                           case 1: viewCustomerAccount view = new viewCustomerAccount(searchCustomer);
+                           break;
+                           case 2: EditAddressPage edit = new EditAddressPage(searchCustomer);
+                           break;
+                       }
+                    }
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
