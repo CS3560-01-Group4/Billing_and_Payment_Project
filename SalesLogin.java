e3f4a6a447d3340f9e04724ed9597ad1bf341043
cyberdremer <dlara7913@gmail.com>
2023-04-30 15:43:45 -0700

    -Spruced up SalesmanLogin form

--- SalesLogin.java
+++ SalesLogin.java
@@ -4,28 +4,22 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
 public class SalesLogin extends JFrame {
-    private JPasswordField passwordField = new JPasswordField(20);
-    private JTextField usernameField = new JTextField(20);
-    private JLabel usernameLabel = new JLabel("Enter Username");
-    private JLabel passwordLabel = new JLabel("Enter Password");
+    private JPasswordField passwordField;
+    private JTextField usernameField;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
     private String retrievedPassword, retreviedUsername;
-    private JButton loginButton = new JButton("Login");
-    private JPanel salesPersonPanel = new JPanel();
-
+    private JButton loginButton;
+    private JPanel salesPersonPanel;
 
     SalesLogin(){
         this.setContentPane(salesPersonPanel);
         this.setTitle("Salesperson Login");
         this.setIconImage(new ImageIcon("31-hour.png").getImage());
         this.setSize(600,600);
-        this.add(loginButton);
-        
         usernameField.setBounds(100,20,165,25);
-        this.add(usernameLabel);
-        this.add(usernameField);
         passwordField.setBounds(100,20,165,25);
-        this.add(passwordLabel);
-        this.add(passwordField);
+
 
         loginButton.addActionListener(new ActionListener() {
             @Override
